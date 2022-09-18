681d467ba5ca9fd8e01b89d976053f52e56f693c
luricheng <707279218@qq.com>
2022-09-18 18:10:54 +0800

    gofmt

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
+	"flag"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
-    "net/http"
-    "flag"
 )
 
 func main() {
-    var formPort, toPort string
+	var formPort, toPort string
 
-    flag.StringVar(&formPort, "f", "1234", "from port")
-    flag.StringVar(&toPort, "t", "1234", "to port")
-    flag.Parse()
+	flag.StringVar(&formPort, "f", "1234", "from port")
+	flag.StringVar(&toPort, "t", "1234", "to port")
+	flag.Parse()
 
-    remote, err := url.Parse("http://127.0.0.1:" + toPort)
-    if err != nil {
-        panic(err)
-    }
+	remote, err := url.Parse("http://127.0.0.1:" + toPort)
+	if err != nil {
+		panic(err)
+	}
 
-    handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-        return func(w http.ResponseWriter, r *http.Request) {
-            r.Host = remote.Host
-            p.ServeHTTP(w, r)
-        }
-    }
-    
-    proxy := httputil.NewSingleHostReverseProxy(remote)
-    http.HandleFunc("/", handler(proxy))
-    err = http.ListenAndServe(":" + formPort, nil)
-    if err != nil {
-        panic(err)
-    }
+	handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+		return func(w http.ResponseWriter, r *http.Request) {
+			r.Host = remote.Host
+			p.ServeHTTP(w, r)
+		}
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	http.HandleFunc("/", handler(proxy))
+	err = http.ListenAndServe(":"+formPort, nil)
+	if err != nil {
+		panic(err)
+	}
 }
